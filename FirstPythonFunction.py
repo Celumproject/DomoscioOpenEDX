7954aae301db4ff97232fe4ee96bb0e4042d8f76
Samir AMZANI <samir.amzani@dailymotion.com>
2015-05-29 19:08:40 +0200

    Added uid to domoscio

--- FirstPythonFunction.py
+++ FirstPythonFunction.py
@@ -1,19 +1,18 @@
-def createKnowledgeNode(content_name) :
+import requests
+import json
+from pprint import pprint
+
+def createKnowledgeNode(uid) :
     # Create a KnowledgeNode given the content parameter
-    import requests
-    import json
-    
-    r = requests.post("http://stats-engine.domoscio.com/v1/companies/32/knowledge_nodes?name=" + content_name + "&knowledge_graph_id=57&token=cd841abb68694c5c7a44d17af1a12582")
+    r = requests.post("http://stats-engine.domoscio.com/v1/companies/32/knowledge_nodes?uid=" + str(uid) + "&knowledge_graph_id=57&token=cd841abb68694c5c7a44d17af1a12582")
     return json.loads(r.text)["id"]
     
                                                 
-def createStudent() :
+def createStudent(uid) :
     # Create a KnowledgeNode given the content parameter
-    import requests
-    import json
-    
     data = {
         "student_group_id": 1,
+        "uid" : uid,
         "civil_profile_attributes": {
             "name": "Nadra",
             "sexe": "female",
@@ -29,40 +28,35 @@ def createStudent() :
     }
 
     r = requests.post("http://stats-engine.domoscio.com/v1/companies/32/students", params=params, json=data)
-    
     return json.loads(r.text)["id"]
 
-def createKnowledgeNodeStudent(student_id) :
+def createKnowledgeNodeStudent(student_id, knowledge_node_id) :
     # Create a KnowledgeNode given the content parameter
-    import requests
-    import json
-    
-    r = requests.post("http://stats-engine.domoscio.com/v1/companies/32/knowledge_node_students?student_id=" + str(student_id) + "&knowledge_node_id=116&token=cd841abb68694c5c7a44d17af1a12582")
-    
+    r = requests.post("http://stats-engine.domoscio.com/v1/companies/32/knowledge_node_students?student_id=%s&knowledge_node_id=%s&token=cd841abb68694c5c7a44d17af1a12582" % (student_id, knowledge_node_id))
     return json.loads(r.text)["id"]
     
 
-def createResult(knowledge_node_student_id, result) :
+def createResult(uid1, uid2, result) :
     # Create a KnowledgeNode given the content parameter
-    import requests
-    import json
-    
-    r = requests.post("http://stats-engine.domoscio.com/v1/companies/32/results?knowledge_node_student_id=" + str(knowledge_node_student_id) + "&value=" + str(result) + "&token=cd841abb68694c5c7a44d17af1a12582")
+    r = requests.post("http://stats-engine.domoscio.com/v1/companies/32/results?knowledge_node_uid=%s&value=%s&token=cd841abb68694c5c7a44d17af1a12582&student_uid=%s" % (uid1, result, uid2))
 
     return json.loads(r.text)["id"]
 
                                                 
-def fetchAllReviews(student_id) :
+def fetchAllReviews(uid) :
     # Fetch all the knowledgeNodes that are to be reviewed
-    import requests
-    import json
-    
-    r = requests.get("http://stats-engine.domoscio.com/v1/companies/32/review_utils/fetch_reviews?student_id=" + str(student_id) + "&token=cd841abb68694c5c7a44d17af1a12582")
-    
+    r = requests.get("http://stats-engine.domoscio.com/v1/companies/32/review_utils/fetch_reviews?student_uid=" + str(uid) + "&token=cd841abb68694c5c7a44d17af1a12582")
+    pprint(r.text)
     return json.loads(r.text)
 
-student_id = createStudent()
-knowledge_node_student_id = createKnowledgeNodeStudent(student_id)
-createResult(knowledge_node_student_id, 1)
+uid = 12323
+student_id = createStudent(uid)
+knowledge_node_id = createKnowledgeNode(uid)
 
-print fetchAllReviews(student_id)
+print "Created student = %s" % student_id
+knowledge_node_uid = createKnowledgeNodeStudent(student_id, knowledge_node_id)
+print "Created knowledge_node_uid = %s" % knowledge_node_uid
+result = createResult(uid, uid, 1)
+print "Created result = %s" % result
+
+print fetchAllReviews(uid)
